135404ad47c4692a98cafa73ffb847c3656170b9
whodedo <dtmc81@gmail.com>
2022-12-25 16:12:51 +0700

    modules examples

---
+++ modules.py
@@ -0,0 +1,29 @@
+# Modules
+
+# import dog
+# dog.bark()
+
+# from dog import bark
+# bark()
+
+import math
+from lib import dog2
+from lib.dog2 import bark
+dog2.bark()
+bark()
+
+
+print(math.sqrt(4))
+
+
+# math for math utilities
+# re for regular expressions
+# json to work with JSON
+# datetime to work with dates
+# sqlite3 to use SQLite
+# os for Operating System utilities
+# random for random number generation
+# statistics for statistics utilities
+# requests to perform HTTP network requests
+# http to create HTTP servers
+# urllib to manage URLs
